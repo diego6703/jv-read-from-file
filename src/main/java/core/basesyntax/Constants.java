945947582092d838e00dd2de19eaf162c0f90f6f
Jakub Cybulski <jakub_cybulski@o2.pl>
2025-09-26 20:49:24 +0200

    Reading from File and Filtering full implementation

---
+++ src/main/java/core/basesyntax/Constants.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public class Constants {
+    public static final String MISSING_FILE_EXCEPTION = "can't find file";
+    public static final String REGEX_SEPARATOR = "\\W+";
+    public static final String LIST_SEPARATOR = " ";
+    public static final char FIRST_LETTER_FOR_SEARCH = 'w';
+    public static final int INDEX_FOR_SEARCH = 0;
+    public static final int EMPTY_STRING_BUILDER_LENGTH = 0;
+}
--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] result = new String[]{};
+        File file = new File(fileName);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file));) {
+            String value = bufferedReader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println(Constants.MISSING_FILE_EXCEPTION);
+        }
+        if (!stringBuilder.isEmpty()) {
+            String[] words = stringBuilder.toString().toLowerCase().split(Constants.REGEX_SEPARATOR);
+            stringBuilder.setLength(Constants.EMPTY_STRING_BUILDER_LENGTH);
+            for (String word : words) {
+                if (word.charAt(Constants.INDEX_FOR_SEARCH) == Constants.FIRST_LETTER_FOR_SEARCH) {
+                    stringBuilder.append(word).append(Constants.LIST_SEPARATOR);
+                }
+            }
+            if (!stringBuilder.isEmpty()) {
+                result = stringBuilder.toString().split(Constants.LIST_SEPARATOR);
+                Arrays.sort(result);
+                return result;
+            }
+        }
+        return result;
     }
 }
